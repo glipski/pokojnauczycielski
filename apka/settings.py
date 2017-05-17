b440cb48dff345810147cc2f222efe5c5541879b
glipski <grzegorzjakublipski@gmail.com>
2017-05-17 12:58:59 +0200

    9

--- apka/settings.py
+++ apka/settings.py
@@ -30,7 +30,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
-ALLOWED_HOSTS = ['https://pokoj-nauczycielski.herokuapp.com/']
+ALLOWED_HOSTS = ['pokoj-nauczycielski.herokuapp.com',]
 
 DEBUG = True
 
