ad68318991c9320e7b2067de0808a875e9493578
glipski <grzegorzjakublipski@gmail.com>
2017-05-17 12:23:23 +0200

    5

--- apka/settings.py
+++ apka/settings.py
@@ -28,6 +28,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+
+
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
@@ -134,4 +136,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
